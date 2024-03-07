81f24f922ac3371ad6a7108173bf8f9d40738098
노우성, WooSung Roh <155697546+RohWS@users.noreply.github.com>
2024-03-07 17:17:01 +0900

    [level 0] Title: 카운트 업, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -0,0 +1,7 @@
+def solution(start_num, end_num):
+    sum2 = []
+    for i in range(end_num - start_num + 1):
+        sum2.append(start_num + i)
+    # print(sum_list)
+    # answer = sum_list
+    return sum2
