5bdd6cdc4a2ce6ee35e5db141b3f209c2fc086e9
노우성, WooSung Roh <155697546+RohWS@users.noreply.github.com>
2024-03-25 12:05:12 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,9 +1,20 @@
-import itertools
+T = [int(input()) for _ in range(9)]
 
-array = [int(input()) for _ in range(9)]
+S = list(T) #copy
+sum_S = sum(S) # > 100
+sub_S = int(sum_S - 100)
 
-for i in itertools.combinations(array, 7):  # 해당 배열을 7명 중복없이 뽑아준다.
-    if sum(i) == 100:  # 그합이 100이라면
-        for j in sorted(i):  # 그 7명을 오름차순으로 정렬후 출력한다.
-            print(j)
-        break #그 후 반복문 탈출
+a = 0
+while a < 9:
+    S = list(T)
+    no_drf2 = int(sub_S - S[a])
+    S.remove(S[a])
+    if no_drf2 in S:
+        S.remove(no_drf2)
+        break
+    else:
+        a += 1
+
+S.sort()
+for i in range(len(S)):
+    print(S[i])
