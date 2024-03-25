99d2f86e2a459304feb90bb3b17b4648f2c6388c
노우성, WooSung Roh <155697546+RohWS@users.noreply.github.com>
2024-03-25 23:49:23 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,23 +1,15 @@
 T = int(input())
-sub_T = 1000 - T
+result = 1000 - T
+
 coin = 0
-while True:
-  if sub_T >= 500:
-    coin += 1
-    sub_T -= 500
-  elif sub_T >= 100:
-    sub_T -= 100
-    coin += 1
-  elif sub_T >= 50:
-    sub_T -= 50
-    coin += 1
-  elif sub_T >= 10:
-    sub_T -= 10
-    coin += 1
-  elif sub_T >= 5:
-    sub_T -= 5
-    coin += 1
-  else:
-    coin += sub_T
+
+money = [500, 100, 50, 10, 5, 1]
+
+for i in money:
+  if result == 0:
     break
+
+  coin += result // i
+  result %= i
+
 print(coin)
